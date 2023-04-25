ecdb341575b61f0e6b7c5234b298cc97e32538a8
livioqueiroz <queiroz.livio@gmail.com>
2023-04-25 19:38:43 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/ecommerceccca/ECommerceCccaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ecommerceccca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceCccaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ECommerceCccaApplication>(*args)
+}
